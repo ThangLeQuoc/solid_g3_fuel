902a3eba2d0c1adbaed26b035cae77d7130d1c0b
Nguyệt Thu Thị Võ <nguyet.vo@adnovum.vn>
2018-06-11 08:57:38 +0700

    Initial Commit

---
+++ src/Bus.java
@@ -0,0 +1,7 @@
+public class Bus extends Vehicle{
+    @Override
+    Long totalOfFuelInDay() {
+        // to be defined
+        return Long.valueOf(0);
+    }
+}
---
+++ src/Truck.java
@@ -0,0 +1,7 @@
+public class Truck extends Vehicle{
+    @Override
+    Long totalOfFuelInDay() {
+        // to be defined
+        return Long.valueOf(0);
+    }
+}
